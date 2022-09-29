9d5a457d92fc6888de0ce739e328f298ab40b4f0
Varga Zsigmond <petrikdiak@gmail.com>
2022-09-29 10:33:27 +0200

    Legtöbb like

--- src/hu/petrik/bejegyzesprojekt/Main.java
+++ src/hu/petrik/bejegyzesprojekt/Main.java
@@ -30,48 +30,64 @@ public class Main {
         likeosztas();
         szovegcsere();
         System.out.println(bejegyzesek);
+        legtobblike();
     }
-        private static void listahozzaad()
-        {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Hány bejegyzést szeretne felvenni?");
-            int db = sc.nextInt();
-            sc.nextLine();
-            for (int i = 0; i < db; i++) {
-                System.out.printf("Adja meg a %d bejegyzés szerzőjét:", i + 1);
-                String szerzo = sc.nextLine();
-                System.out.printf("Adjam eg a %d bejegyzés tartalmát:", i + 1);
-                String tartalom = sc.nextLine();
-                bejegyzesek.add(new Bejegyzes(szerzo, tartalom));
 
+    private static void listahozzaad() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hány bejegyzést szeretne felvenni?");
+        int db = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < db; i++) {
+            System.out.printf("Adja meg a %d bejegyzés szerzőjét:", i + 1);
+            String szerzo = sc.nextLine();
+            System.out.printf("Adjam eg a %d bejegyzés tartalmát:", i + 1);
+            String tartalom = sc.nextLine();
+            bejegyzesek.add(new Bejegyzes(szerzo, tartalom));
+
+        }
+    }
+
+    private static void beolvas(String fajlNev) throws IOException {
+        FileReader fr = new FileReader(fajlNev);
+        BufferedReader br = new BufferedReader(fr);
+        String sor = br.readLine();
+        while (sor != null && !sor.equals("")) {
+            String[] info = sor.split(";");
+            Bejegyzes bejegyzes = new Bejegyzes(info[0], info[1]);
+            bejegyzesek.add(bejegyzes);
+            sor = br.readLine();
+        }
+
+    }
+
+    private static void likeosztas() {
+        Random r = new Random();
+        for (int i = 0; i < bejegyzesek.size() * 20; i++) {
+            bejegyzesek.get(r.nextInt(bejegyzesek.size())).like();
+        }
+    }
+
+    private static void szovegcsere() {
+        String bekertszoveg;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Írd be az új szöveget");
+        bekertszoveg = sc.nextLine();
+
+        bejegyzesek.get(1).setTartalom(bekertszoveg);
+    }
+
+    private static void legtobblike()
+    {
+        int likeok =0;
+        for (int i = 0; i < bejegyzesek.size(); i++) {
+            if ( bejegyzesek.get(i).getLikeok()>likeok)
+            {
+                likeok=bejegyzesek.get(i).getLikeok();
             }
-        }
-        private static void beolvas(String fajlNev) throws IOException {
-            FileReader fr = new FileReader(fajlNev);
-            BufferedReader br = new BufferedReader(fr);
-            String sor = br.readLine();
-            while (sor != null && !sor.equals("")) {
-                String[] info = sor.split(";");
-                Bejegyzes bejegyzes = new Bejegyzes(info[0], info[1]);
-                bejegyzesek.add(bejegyzes);
-                sor = br.readLine();
-            }
 
         }
-
-        private static void likeosztas() {Random r = new Random();
-            for (int i = 0; i < bejegyzesek.size()*20; i++) {
-              bejegyzesek.get(r.nextInt(bejegyzesek.size())).like();
-            }
-        }
-        private  static void szovegcsere()
-        {
-            String bekertszoveg;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Írd be az új szöveget");
-            bekertszoveg=sc.nextLine();
-
-            bejegyzesek.get(1).setTartalom(bekertszoveg);
-        }
+        System.out.println("A legtöbb like száma:" + likeok);
+    }
 
     }
