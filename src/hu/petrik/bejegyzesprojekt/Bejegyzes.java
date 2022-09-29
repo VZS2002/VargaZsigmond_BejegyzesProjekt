8b569de51d8b07bc7219dbd3c278dd2d749eda1a
Varga Zsigmond <petrikdiak@gmail.com>
2022-09-29 09:17:17 +0200

    listahozzaad() létrehozása

--- src/hu/petrik/bejegyzesprojekt/Bejegyzes.java
+++ src/hu/petrik/bejegyzesprojekt/Bejegyzes.java
@@ -9,6 +9,7 @@ public class Bejegyzes {
 
     public void setTartalom(String tartalom) {
         this.tartalom = tartalom;
+        this.szerkesztve = LocalDateTime.now();
     }
 
     private String tartalom;
@@ -19,9 +20,9 @@ public class Bejegyzes {
     public Bejegyzes(String szerzo, String tartalom) {
         this.szerzo = szerzo;
         this.tartalom = tartalom;
-        this.likeok =0;
-        this.letrejott= LocalDateTime.now();
-        this.szerkesztve= LocalDateTime.now();
+        this.likeok = 0;
+        this.letrejott = LocalDateTime.now();
+        this.szerkesztve = LocalDateTime.now();
     }
 
     public String getSzerzo() {
@@ -44,19 +45,17 @@ public class Bejegyzes {
         return szerkesztve;
     }
 
-    public void like()
-    {
+    public void like() {
         this.likeok++;
     }
-    @Override
-    public String toString(){
-        if(this.szerkesztve == this.letrejott)
-        {
-            return String.format("%s - %d -s%\n" + "Szerkesztve:\n" + "%s", this.szerzo, this.likeok, this.letrejott, this.tartalom);
 
-        }
-else {
-    return String.format("%s - %d -s%\n" + "Szerkesztve: %s\n" + "%s", this.szerzo, this.likeok, this.letrejott, this.szerkesztve, this.tartalom);
+    @Override
+    public String toString() {
+        if (this.szerkesztve == this.letrejott) {
+            return String.format("%s - %d - %s\n  Szerkesztve: \n %s", this.szerzo, this.likeok, this.letrejott, this.tartalom);
+
+        } else {
+            return String.format("%s - %d - %s\n Szerkesztve: %s\n %s", this.szerzo, this.likeok, this.letrejott, this.szerkesztve, this.tartalom);
 
         }
     }
--- src/hu/petrik/bejegyzesprojekt/Main.java
+++ src/hu/petrik/bejegyzesprojekt/Main.java
@@ -1,8 +1,31 @@
 package hu.petrik.bejegyzesprojekt;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
+    static List<Bejegyzes> bejegyzesek = new ArrayList<>();
     public static void main(String[] args) {
-	// write your code here
+	bejegyzesek.add(new Bejegyzes("Gipsz Jakab", "Valami tartalom"));
+	bejegyzesek.add(new Bejegyzes("Gipsz Jakab", "Valami új tartalom"));
+    listahozzaad();
+    }
+
+    private  static void listahozzaad()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hány bejegyzést szeretne felvenni?");
+        int db = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < db; i++) {
+            System.out.printf("Adja meg a %d bejegyzés szerzőjét:" , i+1);
+            String szerzo = sc.nextLine();
+            System.out.printf("Adjam eg a %d bejegyzés tartalmát:", i+1);
+            String tartalom = sc.nextLine();
+            bejegyzesek.add(new Bejegyzes(szerzo, tartalom));
+            System.out.println(bejegyzesek);
+        }
     }
 }
